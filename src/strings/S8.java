3ea3fa43982d8c435bce4c9ea4957bdb35e96f28
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2016-12-05 12:26:50 +0530

    Zero Index Refactored

--- src/strings/S8.java
+++ src/strings/S8.java
@@ -1,28 +1,6 @@
 package strings;
 
-import java.util.Scanner;
-
 public class S8 {
-//	private static void S8(Scanner s) {
-//		System.out.print("Enter M: ");
-//		int m = s.nextInt();
-//		System.out.print("Enter N: ");
-//		int n = s.nextInt();
-//		int[][] a = new int[m][n];
-//		System.out.println("Enter matrix:");
-//		for (int i = 0; i < m; i++)
-//			for (int j = 0; j < n; j++) {
-//				System.out.print(i + "\t" + j+" : ");
-//				a[i][j] = s.nextInt();
-//			}
-//		S8.ZeroMatrix(a, m, n);
-//		for (int i = 0; i < m; i++) {
-//			for (int j = 0; j < n; j++)
-//				System.out.print(a[i][j] + "\t");
-//			System.out.println();
-//		}
-//	}
-
 	public static void ZeroMatrix(int[][] a, int m, int n) {
 		boolean[] z = new boolean[n];
 		boolean flag = false;
