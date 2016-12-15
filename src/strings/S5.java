7a20c2552cd2447430d97cc75c7d8f6dfdd0be7d
YashxRaj <fantastic.yash@gmail.com>
2016-12-16 02:29:38 +0530

    Update S5.java

--- src/strings/S5.java
+++ src/strings/S5.java
@@ -1,29 +1,33 @@
 package strings;
 
-import java.util.HashMap;
-
 public class S5 {
-
+	// FYI you did this drunk, so next time, BELIEVE IN YOURSELF!
 	public static Boolean OneAway(String one, String two) {
-		int l = one.length() - two.length() > 0 ? one.length() : two.length(), ctr = 0;
-		if (Math.abs(l) > 1)
+		int onel = one.length(), twol = two.length(), l = onel - twol, ctr = 0;
+		boolean flag = true;
+		if (l < 0)
+			flag = false;
+		if (l == 0) {
+			for (int i = 0; i < onel; i++) {
+				if (one.charAt(i) != two.charAt(i)) {
+					ctr++;
+				}
+			}
+			return ctr < 2;
+		} else if (Math.abs(l) == 1) {
+			String s1 = one, s2 = two;
+			if (flag == false) {
+				s1 = two;
+				s2 = one;
+			}
+			for (int i = 0; i < s2.length(); i++) {
+				if(s1.charAt(i)==s2.charAt(i))
+					ctr++;
+				else if(s1.charAt(i+1) == s2.charAt(i))
+					ctr++;
+			}
+			return ctr == s2.length();
+		} else
 			return false;
-		HashMap<Character, Integer> m = new HashMap<Character, Integer>();
-		String s1 = one, s2 = two;
-		if (l == two.length()) {
-			s1 = two;
-			s2 = one;
-		}
-		for (int i = 0; i < l; i++) {
-			if (m.get(s1.charAt(i)) == null)
-				m.put(s1.charAt(i), 1);
-			else
-				m.put(s1.charAt(i), m.get(s1.charAt(i)) + 1);
-		}
-		for (int i = 0; i < s2.length(); i++) {
-			if (!(m.containsKey(s2.charAt(i))))
-				ctr++;
-		}
-		return ctr < 2;
 	}
 }
