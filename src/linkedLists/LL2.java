7e63c8d2e1530a7f71f326ab92badf4fecf2c33d
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2016-12-20 15:58:48 +0530

    Linked List Second Question

--- src/linkedLists/LL2.java
+++ src/linkedLists/LL2.java
@@ -3,17 +3,21 @@ package linkedLists;
 public class LL2 {
 
 	public static void kthToLastElement(Node head, int k) {
-		Node n = head;
-		Node newHead = null;
+		if (k > head.size()) {
+			LL.printLL(head, "Entered value for K is larger than the size of the list. Printing list:");
+			return;
+		}
+		Node n = head, newHead = null;
 		int l = 0;
 		while (n != null) {
-			l++;
-			if (l == k) {
-				head = newHead;
-				newHead = head;
-				LL.printLL(newHead, "K-th element to last element:");
+			if (l != (k - 1)) {
+				l++;
+				n = n.next;
+				continue;
 			}
+			newHead = n;
+			LL.printLL(newHead, "K-th element to last element:");
+			break;
 		}
 	}
-
 }
