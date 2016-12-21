685876ddcf4cb47ec4503f79fe57582f61c9920b
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2016-12-21 20:09:01 +0530

    Working on followup

--- src/linkedLists/LL5.java
+++ src/linkedLists/LL5.java
@@ -3,7 +3,8 @@ package linkedLists;
 public class LL5 {
 
 	public static Node sum(Node one, Node two) {
-		return makeReverseLLFromInt((makeIntFromLL(one)) + makeIntFromLL(two));
+
+		return makeReverseLLFromInt((makeIntFromReverseLL(one)) + makeIntFromReverseLL(two));
 	}
 
 	private static Node makeReverseLLFromInt(int sum) {
@@ -16,7 +17,7 @@ public class LL5 {
 		return s;
 	}
 
-	private static int makeIntFromLL(Node n) {
+	private static int makeIntFromReverseLL(Node n) {
 		int val = 0, i = 1;
 		while (n != null) {
 			val += n.data * i;
@@ -26,38 +27,38 @@ public class LL5 {
 		return val;
 	}
 
-	/*
-	 * private static Node makeLLFromInt(int res) {
-	 * Node s = null;
-	 * while (res != 0) {
-	 * int d = res % 10;
-	 * if (s == null)
-	 * s = new Node(d);
-	 * else {
-	 * Node temp = new Node(d);
-	 * temp.next = s;
-	 * s = temp;
-	 * }
-	 * res /= 10;
-	 * }
-	 * return s;
-	 * }
-	 * private static int[] makeArrayfromLL(Node n) {
-	 * int i = 0;
-	 * int[] a = new int[n.size()];
-	 * while (n != null) {
-	 * a[i++] = n.data;
-	 * n = n.next;
-	 * }
-	 * return a;
-	 * }
-	 *
-	 * private static int makeIntFromArray(int[] a) {
-	 * int val = 0;
-	 * for (int i = 0, j = 1; i < a.length; i++, j *= 10) {
-	 * val += a[i] * j;
-	 * }
-	 * return val;
-	 * }
-	 */
+	private static Node makeLLFromInt(int res) {
+		Node s = null;
+		while (res != 0) {
+			int d = res % 10;
+			if (s == null)
+				s = new Node(d);
+			else {
+				Node temp = new Node(d);
+				temp.next = s;
+				s = temp;
+			}
+			res /= 10;
+		}
+		return s;
+	}
+
+	private static int[] makeArrayfromLL(Node n) {
+		int i = 0;
+		int[] a = new int[n.size()];
+		while (n != null) {
+			a[i++] = n.data;
+			n = n.next;
+		}
+		return a;
+	}
+
+	private static int makeIntFromArray(int[] a) {
+		int val = 0;
+		for (int i = 0, j = 1; i < a.length; i++, j *= 10) {
+			val += a[i] * j;
+		}
+		return val;
+	}
+
 }
