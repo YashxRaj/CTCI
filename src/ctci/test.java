182cdbe21835d6a0779ea6b58f6df2f7ba9898e0
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2016-12-26 20:29:37 +0530

    test

--- src/ctci/test.java
+++ src/ctci/test.java
@@ -1,14 +1,11 @@
-package CTCI;
+package ctci;
+
+import stacksAndQueues.Stack;
 
 public class test {
 
 	public static void main(String[] args) {
-		int a = 4;
-		int b = 4;
-		if (a <= b)
-			System.out.println(a + " < " + b);
-		else if (a >= b)
-			System.out.println(a + " > " + b);
+		stacksAndQueues.Stack<Integer> s = new Stack<Integer>();
 	}
 
 }
