a35a51b07dab490f0002839c2b43f9befc25d9e3
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2016-12-26 20:07:59 +0530

    Test

---
+++ src/CTCI/test.java
@@ -0,0 +1,14 @@
+package CTCI;
+
+public class test {
+
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 4;
+		if (a <= b)
+			System.out.println(a + " < " + b);
+		else if (a >= b)
+			System.out.println(a + " > " + b);
+	}
+
+}
