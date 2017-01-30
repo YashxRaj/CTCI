8e3fcb52d07ab3a750a1f9929106f8ff15ee398a
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2017-01-30 17:57:21 +0530

    Hello World!

--- src/ctci/test.java
+++ src/ctci/test.java
@@ -1,11 +1,10 @@
 package ctci;
 
-import stacksAndQueues.Stack;
 
 public class test {
 
 	public static void main(String[] args) {
-		stacksAndQueues.Stack<Integer> s = new Stack<Integer>();
+		System.out.println("Hello World!");
 	}
 
 }
