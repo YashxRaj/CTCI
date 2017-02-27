1f98e6efb6c97ad90e97f103b27e19a4d3c1ca6a
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2017-02-27 14:46:05 +0530

    getBit

--- src/ctci/test2.java
+++ src/ctci/test2.java
@@ -1,19 +1,14 @@
 package ctci;
 
-import java.util.HashMap;
-
 public class test2 {
 
 	public static void main(String[] args) {
-		int a = 80;
-		HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
-		System.out.println(Integer.toBinaryString(a));
-		System.out.println(Integer.toHexString(a));
-		System.out.println(Integer.toOctalString(a));
-		System.out.println(Integer.toString(a));
-		System.out.println(Integer.toString(a, 2));
-		System.out.println(Integer.toString(a, 8));
-		System.out.println(Integer.toString(a, 16));
-		System.out.println(Integer.toString(a, 64));
+		int num = 21;
+		for (int i = 0; i < String.valueOf(Integer.toBinaryString(num)).length(); i++)
+			System.out.println(getBit(num, i));
+	}
+
+	private static int getBit(int num, int i) {
+		return ((num & (1 << i)) != 0) == false ? 0 : 1;
 	}
 }
