a04710a344d6edc94b19c2238c37d2e94588edb0
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2017-03-02 17:58:37 +0530

    WIP

--- src/nonCTCI/PalindromicPartitions.java
+++ src/nonCTCI/PalindromicPartitions.java
@@ -1,9 +1,50 @@
 package nonCTCI;
 
+import java.util.Scanner;
+
 public class PalindromicPartitions {
 	// http://www.geeksforgeeks.org/given-a-string-print-all-possible-palindromic-partition/
 	public static void main(String[] args) {
-		
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter String: ");
+		String str = s.nextLine();
+		System.out.println("Palindromes: ");
+		palindromePartitions(str);
 	}
 
+	private static void palindromePartitions(String str) {
+		char[] c = str.toCharArray();
+		for (char x : c)
+			System.out.print(x + " ");
+		System.out.println(); // Printing all characters.
+		for (int i = 1; i < c.length - 1; i++) {
+			StringBuilder s = new StringBuilder();
+			if (c[i] == c[i - 1]) {
+				if (c[i - 1] != c[i + 1]) {
+					s.append(c[i]);
+					s.append(c[i - 1]);
+					System.out.println(s.toString());
+				} else {
+					int j = 1;
+					while (i + j < c.length && c[i + j] == c[i - j])
+						j++;
+					print(c, i, j);
+				}
+			} else {
+				s.append(c[i]);
+				s.append(" ");
+				System.out.println(s.toString());
+			}
+		}
+	}
+
+	private static void print(char[] c, int i, int j) {
+		StringBuilder s = new StringBuilder();
+		String str = c[i] + "";
+		for (int k = 1; k < j; k++) {
+			s.append(c[i-j]);
+			s.append(str);
+			s.append(c[i+j]);
+		}
+	}
 }
