7c55ee6ba97de5d49fbd72ebaadc764973d4661e
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2017-03-21 14:48:44 +0530

    Working as expected. Correctly.

--- src/linkedLists/LL2.java
+++ src/linkedLists/LL2.java
@@ -3,27 +3,23 @@ package linkedLists;
 public class LL2 {
 
 	public static void kthToLastElement(Node head, int k) {
-		if (k > head.size()) {
-			LL.printLL(head, "Entered value for K is larger than the size of the list. Printing list:");
-			return;
-		}
-		Node n = head, kth = null;
-		int l = 0;
-		while (n != null) {
-			if (l < k) {
+		int size = head.size();
+		if (k > size) {
+			System.out.println("Entered value for K is larger than the size of the list. Printing list:");
+		} else if (k == size) {
+			System.out.println("Printing Head: " + head.data);
+		} else {
+			Node n = head, kth = head;
+			int l = 0;
+			while (l != k) {
 				l++;
 				n = n.next;
-				continue;
-			} else if (l == k) {
-				kth = n;
-				l++;
-				n = n.next;
-				continue;
-			} else if (l > k) {
+			}
+			while (n.next != null) {
 				kth = kth.next;
 				n = n.next;
 			}
+			System.out.println(k + "-th Element from last: " + kth.data);
 		}
-		System.out.println(k + "-th Element from last: " + kth.data);
 	}
 }
