6e08d9e60d56409f2be716f2fa7f3ab6de197708
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2017-03-21 18:33:49 +0530

    k-th from last complete

--- src/linkedLists/LL2.java
+++ src/linkedLists/LL2.java
@@ -3,23 +3,16 @@ package linkedLists;
 public class LL2 {
 
 	public static void kthToLastElement(Node head, int k) {
-		int size = head.size();
-		if (k > size) {
-			System.out.println("Entered value for K is larger than the size of the list. Printing list:");
-		} else if (k == size) {
-			System.out.println("Printing Head: " + head.data);
-		} else {
-			Node n = head, kth = head;
-			int l = 0;
-			while (l != k) {
-				l++;
-				n = n.next;
-			}
+		Node n = head, kth = head;
+		for (int i = 0; i != k; i++)
+			n = n.next;
+		if (n == null)
+			System.out.println("K > list size");
+		else
 			while (n.next != null) {
 				kth = kth.next;
 				n = n.next;
 			}
-			System.out.println(k + "-th Element from last: " + kth.data);
-		}
+		System.out.println(k + "-th Element from last: " + kth.data);
 	}
 }
