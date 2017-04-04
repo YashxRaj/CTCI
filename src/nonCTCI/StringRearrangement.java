4e29c46f420e60ad20748ac0976100e206c2a432
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2017-04-04 15:51:46 +0530

    No 2 adjacent characters are the same

--- src/nonCTCI/StringRearrangement.java
+++ src/nonCTCI/StringRearrangement.java
@@ -1,9 +1,8 @@
 package nonCTCI;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
-public class StringShuffling {
+public class StringRearrangement {
 	// http://www.geeksforgeeks.org/rearrange-characters-string-no-two-adjacent/
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
@@ -13,15 +12,18 @@ public class StringShuffling {
 	}
 
 	public static String rearrange(String str) {
-		HashMap<Character, Integer> charCount = new HashMap<Character, Integer>();
-		char[] ch = str.toCharArray();
-		for (char x : ch)
-			charCount.put(x, charCount.containsKey(x) ? charCount.get(x) + 1 : 1);
-		StringBuilder s = new StringBuilder();
-		for (Character x : charCount.keySet()) {
-			
-		}
-		return null;
+		char[] c = str.toCharArray();
+		for (int i = 1, k = i; i < c.length; i++)
+			if (c[i] == c[i - 1]) {
+				while (k < c.length && c[k] == c[i])
+					k++;
+				if (k >= c.length)
+					return "Not Possible";
+				char temp = c[i];
+				c[i] = c[k];
+				c[k] = temp;
+			}
+		return new String(c);
 	}
 
 }
