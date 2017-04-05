0c13f052cab9508dd43692cb47e3264451d62906
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2017-04-05 13:32:37 +0530

    Recaman Sequence

--- src/nonCTCI/RecamanSequence.java
+++ src/nonCTCI/RecamanSequence.java
@@ -1,13 +1,38 @@
 package nonCTCI;
 
 import java.util.Scanner;
+import java.util.Vector;
 
 public class RecamanSequence {
 	// http://www.geeksforgeeks.org/recamans-sequence/
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		
+		System.out.print("Enter n: ");
+		Vector<Integer> rs = recamanSequence(s.nextInt());
+		printVector(rs);
 		s.close();
 	}
 
+	public static void printVector(Vector<Integer> rs) {
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < rs.size(); i++)
+			s.append(rs.get(i) + " ");
+		System.out.println(s.toString());
+	}
+
+	public static Vector<Integer> recamanSequence(int n) {
+		Vector<Integer> rs = new Vector<Integer>();
+		rs.add(0);
+		for (int i = 1; i < n; i++)
+			rs.add(rs.get(i - 1) - i >= 0 && !rs.contains(rs.get(i - 1) - i) ? rs.get(i - 1) - i : rs.get(i - 1) + i);
+		return rs;
+	}
 }
+/*
+ * a(0) = 0,
+ * if n > 0 and the number is not
+ * already included in the sequence,
+ * a(n) = a(n - 1) - n
+ * else
+ * a(n) = a(n-1) + n.
+ */
