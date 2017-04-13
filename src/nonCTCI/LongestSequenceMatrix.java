a6d887e4e765a46e8e953112f85c48de39f8cf71
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2017-04-13 13:31:03 +0530

    WIP

--- src/nonCTCI/LongestSequenceMatrix.java
+++ src/nonCTCI/LongestSequenceMatrix.java
@@ -1,6 +1,5 @@
 package nonCTCI;
 
-import java.io.BufferedReader;
 import java.util.Scanner;
 
 public class LongestSequenceMatrix {
@@ -9,31 +8,63 @@ public class LongestSequenceMatrix {
 		Scanner s = new Scanner(System.in);
 		System.out.print("Enter n: ");
 		int n = s.nextInt(), ul = (int) Math.pow(n, 2), ll = 1;
-		System.out.println("Enter the integers ranging from " + ll + " to " + ul + " in any order here.");
+		System.out.println("Enter the integers ranging from " + ll + " to " + ul + " in any order.");
 		int[][] m = new int[n][n];
 		for (int i = 0; i < n; i++)
 			for (int j = 0; j < n; j++) {
 				System.out.print("[" + i + "," + j + "]" + ":");
-				m[i][j] = validateEntry(i, j, ll, ul, s);
+				m[i][j] = validateEntry(i, j, ll, ul, s, m);
 			}
 		s.close();
-		longestPathMatrix(n, m);
+		longestPathMatrix(m);
 	}
 
-	private static int validateEntry(int i, int j, int ll, int ul, Scanner s) {
-		int temp = s.nextInt();
-		if (temp < ll || temp > ul)
+	private static int validateEntry(int i, int j, int ll, int ul, Scanner s, int[][] m) {
+		int temp = entryInRange(i, j, ll, ul, s, m);
+		if (uniqueEntry(temp, m, i, j))
 			return temp;
+		else
+			validateEntry(i, j, ll, ul, s, m);
+		return temp;
+	}
+
+	private static int entryInRange(int i, int j, int ll, int ul, Scanner s, int[][] m) {
+		int temp = s.nextInt();
 		while (temp < ll || temp > ul) {
-			System.out.println("Invalid entry for [" + i + "," + "j].");
-			System.out.println("Please enter values from " + ll + "to " + ul + " only.");
+			System.out.print("Invalid entry for [" + i + "," + j + "]. ");
+			System.out.println("Please enter unique values from " + ll + " to " + ul + " only.");
 			System.out.print("[" + i + "," + j + "]" + ":");
 			temp = s.nextInt();
 		}
 		return temp;
 	}
 
-	private static void longestPathMatrix(int n, int[][] m) {
+	private static boolean uniqueEntry(int temp, int[][] m, int i_ul, int j_ul) {
+		for (int i = 0; i <= i_ul; i++)
+			for (int j = 0; j <= j_ul; j++)
+				if (m[i][j] == temp)
+					return false;
+		return true;
+	}
+
+	private static void longestPathMatrix(int[][] m) {
+		int n = m.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				int temp = m[i][j] + 1;
+				if (j + 1 < n && m[i][j + 1] == temp) {
+
+				} else if (j >= 1 && m[i][j - 1] == temp) {
+
+				} else if (i + 1 < n && m[i + 1][j] == temp) {
+
+				} else if (i >= 1 && m[i - 1][j] == temp) {
+
+				} else {
+
+				}
+			}
+		}
 		return;
 	}
 
