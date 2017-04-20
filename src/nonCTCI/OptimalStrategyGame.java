9711d31e0d4230abb878abbacdb97b80fcb72610
ab69170 <ab69170@SGH243RWMT.ctl.intranet>
2017-04-20 15:55:31 +0530

    valid coinPouch creation complete.

---
+++ src/nonCTCI/OptimalStrategyGame.java
@@ -0,0 +1,73 @@
+package nonCTCI;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class OptimalStrategyGame {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		HashSet<Integer> denomenations = getDenomenations(s);
+		System.out.println(print(denomenations));
+		Deque<Integer> coinPouch = fillCoins(denomenations, s);
+		System.out.println(peekInto(coinPouch));
+		s.close();
+	}
+
+	private static String peekInto(Deque<Integer> coinStack) {
+		StringBuilder s = new StringBuilder();
+		s.append("Coins in pouch:" + System.lineSeparator() + "|");
+		for (Integer coin : coinStack)
+			s.append(coin + "|");
+		s.append(System.lineSeparator());
+		return s.toString();
+	}
+
+	private static Deque<Integer> fillCoins(HashSet<Integer> denomenations, Scanner s) {
+		System.out.print("Enter coins: ");
+		Deque<Integer> coinPouch = new ArrayDeque<Integer>();
+		String digits = "\\d+", maybeCoin = s.nextLine();
+		Integer coin = null;
+		while (maybeCoin.matches(digits)) {
+			coin = Integer.parseInt(maybeCoin);
+			if (isDenomenation(denomenations, coin)) {
+				coinPouch.add(coin);
+				System.out.print("Coin Added. Next Coin: ");
+			} else
+				System.out.print("Coins must be valid denomenation. Re-enter: ");
+			maybeCoin = s.nextLine();
+		}
+		System.out.println("Coin pouch filled.");
+		return coinPouch;
+	}
+
+	private static HashSet<Integer> getDenomenations(Scanner s) {
+		System.out.println("Enter the denomenations of the coins:");
+		HashSet<Integer> denomenations = new HashSet<Integer>();
+		String digits = "\\d+", coin = s.nextLine();
+		while (coin.matches(digits)) {
+			denomenations.add(Integer.parseInt(coin));
+			coin = s.nextLine();
+		}
+		return denomenations;
+	}
+
+	private static String print(HashSet<Integer> denomenations) {
+		StringBuilder s = new StringBuilder();
+		s.append("Entered Denomenations:" + System.lineSeparator() + "|");
+		for (Integer x : denomenations)
+			s.append(x + "|");
+		s.append(System.lineSeparator());
+		return s.toString();
+	}
+
+	private static boolean isDenomenation(HashSet<Integer> denomenations, Integer coin) {
+		for (Integer coinType : denomenations)
+			if (coin.equals(coinType))
+				return true;
+		return false;
+	}
+
+}
