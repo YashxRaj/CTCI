e11bc4c8e554527d9d11feea1b670c9e4c6c83aa
ab69170 <ab69170@SGH243RWPF.ctl.intranet>
2017-10-18 15:50:04 +0530

    < O(n)

--- src/microsoft/AnagramSubstringSearch.java
+++ src/microsoft/AnagramSubstringSearch.java
@@ -24,32 +24,32 @@ public class AnagramSubstringSearch {
 		bufferMap.put(newChar, bufferMap.containsKey(newChar) ? bufferMap.get(newChar) + 1 : 1);
 	}
 
-	private static boolean isAnagram(HashMap<Character, Integer> bufferMap, HashMap<Character, Integer> pattern) {
-		for (Character x : pattern.keySet())
-			if (pattern.get(x) != bufferMap.get(x))
+	private static boolean isAnagram(HashMap<Character, Integer> bufferMap, HashMap<Character, Integer> patternMap) {
+		for (Character x : patternMap.keySet())
+			if (patternMap.get(x) != bufferMap.get(x))
 				return false;
 		return true;
 	}
 
 	// My twist on Rabin-Karp Algorithm to make it simpler.
-	private static Integer[] searchRabinKarpAlgorithm(char[] txt, char[] pat) {
-		char[] buffer = new char[pat.length];
-		Vector<Integer> vi = new Vector<Integer>();
-		HashMap<Character, Integer> pattern = new HashMap<Character, Integer>();
+	private static Integer[] searchRabinKarpAlgorithm(char[] txt, char[] pattern) {
+		char[] buffer = new char[pattern.length];
+		Vector<Integer> indices = new Vector<Integer>();
+		HashMap<Character, Integer> patternMap = new HashMap<Character, Integer>();
 		HashMap<Character, Integer> bufferMap = new HashMap<Character, Integer>();
-
-		for (int i = 0; i < pat.length; i++) {
+		// Preparation.
+		for (int i = 0; i < pattern.length; i++) {
 			buffer[i] = txt[i];
-			pattern.put(pat[i], pattern.containsKey(pat[i]) ? pattern.get(pat[i]) + 1 : 1);
+			patternMap.put(pattern[i], patternMap.containsKey(pattern[i]) ? patternMap.get(pattern[i]) + 1 : 1);
 			bufferMap.put(buffer[i], bufferMap.containsKey(buffer[i]) ? bufferMap.get(buffer[i]) + 1 : 1);
 		}
-
+		// Search and update.
 		for (int i = 0; i < txt.length - buffer.length + 1; i++) {
-			if (isAnagram(bufferMap, pattern))
-				vi.add(i);
+			if (isAnagram(bufferMap, patternMap))
+				indices.add(i);
 			if (i != txt.length - buffer.length)
 				shiftAndUpdateMap(buffer, txt[buffer.length + i], bufferMap);
 		}
-		return vi.toArray(new Integer[vi.size()]);
+		return indices.toArray(new Integer[indices.size()]);
 	}
 }
